b119ba2c620de90d0eafdbf6d15b876d46fe21e2
DataTriny <datatriny@gmail.com>
2023-06-17 21:57:23 +0200

    feat: Add CMake support to C bindings

---
+++ bindings/c/accesskit-config.cmake
@@ -0,0 +1,52 @@
+include("${CMAKE_CURRENT_LIST_DIR}/accesskit.cmake")
+
+add_library(accesskit INTERFACE)
+
+add_library(accesskit-static STATIC IMPORTED GLOBAL)
+find_library(_accesskit_static_lib accesskit "${ACCESSKIT_LIBRARIES_DIR}/static")
+set_property(
+    TARGET accesskit-static
+    PROPERTY IMPORTED_LOCATION "${_accesskit_static_lib}"
+)
+if (_accesskit_os STREQUAL "macos")
+    set_property(
+        TARGET accesskit-static
+        PROPERTY INTERFACE_LINK_DIRECTORIES "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
+    )
+elseif (_accesskit_os STREQUAL "windows")
+    set_property(
+        TARGET accesskit-static
+        PROPERTY INTERFACE_LINK_LIBRARIES bcrypt ntdll uiautomationcore userenv ws2_32
+    )
+endif()
+
+add_library(accesskit-shared SHARED IMPORTED GLOBAL)
+find_library(_accesskit_implib accesskit "${ACCESSKIT_LIBRARIES_DIR}/shared")
+if (_accesskit_os STREQUAL "macos")
+    set_property(
+        TARGET accesskit-shared
+        PROPERTY INTERFACE_LINK_DIRECTORIES "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
+    )
+elseif (_accesskit_os STREQUAL "windows")
+    set_property(
+        TARGET accesskit-shared
+        PROPERTY IMPORTED_IMPLIB "${_accesskit_implib}"
+    )
+endif()
+if (_accesskit_os STREQUAL "linux")
+    set(_accesskit_shared_lib "libaccesskit.so")
+elseif (_accesskit_os STREQUAL "macos")
+    set(_accesskit_shared_lib "libaccesskit.dylib")
+elseif (_accesskit_os STREQUAL "windows")
+    set(_accesskit_shared_lib "accesskit.dll")
+endif()
+set_property(
+    TARGET accesskit-shared
+    PROPERTY IMPORTED_LOCATION "${ACCESSKIT_LIBRARIES_DIR}/shared/${_accesskit_shared_lib}"
+)
+
+if (BUILD_SHARED_LIBS)
+    target_link_libraries(accesskit INTERFACE accesskit-shared)
+else()
+    target_link_libraries(accesskit INTERFACE accesskit-static)
+endif()
